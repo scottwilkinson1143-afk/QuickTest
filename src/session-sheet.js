c90cf61f0ca711f18e95fdabb5258a4b6b9d7cb6
scottwilkinson1143-afk <scott.wilkinson1143@googlemail.com>
2025-12-19 10:21:17 +0000

    Add files via upload

--- src/session-sheet.js
+++ src/session-sheet.js
@@ -37,28 +37,5 @@ login().then(() => {
         webIntegrationId: config.webIntegrationId
     } );
     //Load js/qlik after authentication is successful
-    require( ["js/qlik"], function ( qlik ) {
-        qlik.on( "error", function ( error ) {
-            $( '#popupText' ).append( error.message + "<br>" );
-            $( '#popup' ).fadeIn( 1000 );
-        } );
-        $( "#closePopup" ).click( function () {
-            $( '#popup' ).hide();
-        } );
-        //open apps -- inserted here --
-        document.getElementById("id01").innerHTML = 'Initialising App....';
-        const sessionAppFromApp = qlik.sessionAppFromApp("2649b403-fe69-43d9-9cc7-91b4ac7b2e6e", config);
-        console.log(sessionAppFromApp.id);
-        document.getElementById("id01").innerHTML = 'Reloading App....';
-        qlik.theme.apply('Matrix');
-        sessionAppFromApp.doReload().then((result)=>{
-            if( result ){
-                document.getElementById("id01").innerHTML = 'Retrieve Sheet....';
-                sessionAppFromApp.getObject('QV01', 'tAwLnxh');
-            } 
-            else {
-                console.log('Reload failed');
-            }
-        });
-    } );
+
 });
